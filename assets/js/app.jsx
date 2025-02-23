b7316fc6ffd68fafb5ad034ffe3fa77206226f00
Nicholas Hammond <nhammond649@gmail.com>
2025-02-24 09:35:33 +1100

    Add Phoenix channels to project

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { createContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from "axios";
 
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import Layout from "./components/layout";
+
+import socket from "./user_socket"
+import { Socket } from "phoenix"
+
+import {PhoenixSocketProvider, PhoenixSocketContext} from "./lib/phoenix_socket_context"
+
+
+
 
 axios.defaults.xsrfHeaderName = "x-csrf-token";
 
@@ -15,9 +24,15 @@ createInertiaApp({
     const page = await import(`./pages/${name}.tsx`);
     page.default.layout =
       page.default.layout || ((page) => <Layout children={page} />);
+        
     return page;
   },
   setup({ App, el, props }) {
-    createRoot(el).render(<App {...props} />);
+
+    createRoot(el).render(
+      <PhoenixSocketProvider>
+        <App { ...props } />
+      </PhoenixSocketProvider>
+    );
   },
 });
